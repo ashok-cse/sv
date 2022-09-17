336709b5891d6d9cd08e9b3c7a939500c626b614
Ashok <ashokmarmath8@gmail.com>
2022-09-17 17:52:49 +0530

    dfghj

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
+
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,6 +12,7 @@ const config = {
 	],
 	kit: {
 		adapter: adapter()
+
 	}
 }
 
